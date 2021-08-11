fe1700737ad550ea9f6017eca7e9ececb923e84f
zubairbinferdous <zubairbinf@gmail.com>
2021-08-11 20:41:03 +0600

    ok done

---
+++ main.js
@@ -0,0 +1,24 @@
+// 0
+// ""
+// undefined
+// null 
+// NaN
+//flase
+
+//********************//
+
+//true
+// '0'
+// ' '
+// {}
+//[]
+
+// const gF = 'zubair bin ferdous';
+
+let Name = "ok done" ; 
+
+if(Name || Name == 0 ){
+    console.log('condition is true');
+}else{
+    console.log('condition is false');
+}
