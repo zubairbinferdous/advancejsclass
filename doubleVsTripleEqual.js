d0035acea86cd6d193964e66f03f384b25410bfe
zubairbinferdous <zubairbinf@gmail.com>
2021-08-25 23:54:19 +0600

    ok

---
+++ doubleVsTripleEqual.js
@@ -0,0 +1,7 @@
+const first = "two";
+const second = 'two';
+if (first === second) {
+    console.log('this is true');
+}else{
+    console.log('this is false');
+}
---
+++ map-filter.js
@@ -0,0 +1,9 @@
+const numbers = [3 , 8 , 4 , 5];
+const output = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
