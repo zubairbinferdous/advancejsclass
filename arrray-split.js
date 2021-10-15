f2debd9eb9f56c8d85cfbf5084d6b9a4d100eb0b
zubairbinferdous <zubairbinf@gmail.com>
2021-10-15 09:38:48 +0600

    ok

---
+++ arrray-split.js
@@ -0,0 +1,8 @@
+const nums = [1,2,3,4,5,6,7,8];
+//const part = sum.slice(2 , 5);
+const remove = nums.splice(1,4 , 55 );
+//console.log(part);
+// console.log(remove);
+// console.log(nums);
+const join = nums.join(',');
+console.log(join);
